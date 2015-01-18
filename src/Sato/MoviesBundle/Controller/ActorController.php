db0b15980ba94e40dba9413cc0155997d2f4401b
Luciana Sato <sato.lu@gmail.com>
2015-01-18 16:48:51 -0200

    Adicionado melhorias aos layouts

--- src/Sato/MoviesBundle/Controller/ActorController.php
+++ src/Sato/MoviesBundle/Controller/ActorController.php
@@ -18,230 +18,237 @@ use Sato\MoviesBundle\Form\ActorType;
 class ActorController extends Controller
 {
 
-    /**
-     * Lists all Actor entities.
-     *
-     * @Route("/", name="admin_actor")
-     * @Method("GET")
-     * @Template()
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+	/**
+	 * Lists all Actor entities.
+	 *
+	 * @Route("/", name="admin_actor")
+	 * @Method("GET")
+	 * @Template()
+	 */
+	public function indexAction(Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('SatoMoviesBundle:Actor')->findAll();
+		$sql = "SELECT a FROM SatoMoviesBundle:Actor a";
+		$query = $em->createQuery($sql);
 
-        return array(
-            'entities' => $entities,
-        );
-    }
-    /**
-     * Creates a new Actor entity.
-     *
-     * @Route("/", name="admin_actor_create")
-     * @Method("POST")
-     * @Template("SatoMoviesBundle:Actor:new.html.twig")
-     */
-    public function createAction(Request $request)
-    {
-        $entity = new Actor();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
+		$paginator  = $this->get('knp_paginator');
+		$pagination = $paginator->paginate(
+			$query,
+			$request->query->get('page', 1),
+			5
+		);
+		return array(
+			'entities' => $pagination,
+		);
+	}
+	/**
+	 * Creates a new Actor entity.
+	 *
+	 * @Route("/", name="admin_actor_create")
+	 * @Method("POST")
+	 * @Template("SatoMoviesBundle:Actor:new.html.twig")
+	 */
+	public function createAction(Request $request)
+	{
+		$entity = new Actor();
+		$form = $this->createCreateForm($entity);
+		$form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($entity);
+			$em->flush();
 
-            return $this->redirect($this->generateUrl('admin_actor_show', array('id' => $entity->getId())));
-        }
+			return $this->redirect($this->generateUrl('admin_actor_show', array('id' => $entity->getId())));
+		}
 
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
-    }
+		return array(
+			'entity' => $entity,
+			'form'   => $form->createView(),
+		);
+	}
 
-    /**
-     * Creates a form to create a Actor entity.
-     *
-     * @param Actor $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Actor $entity)
-    {
-        $form = $this->createForm(new ActorType(), $entity, array(
-            'action' => $this->generateUrl('admin_actor_create'),
-            'method' => 'POST',
-        ));
+	/**
+	 * Creates a form to create a Actor entity.
+	 *
+	 * @param Actor $entity The entity
+	 *
+	 * @return \Symfony\Component\Form\Form The form
+	 */
+	private function createCreateForm(Actor $entity)
+	{
+		$form = $this->createForm(new ActorType(), $entity, array(
+			'action' => $this->generateUrl('admin_actor_create'),
+			'method' => 'POST',
+		));
 
-        $form->add('submit', 'submit', array('label' => 'Create'));
+		$form->add('submit', 'submit', array('label' => 'Create'));
 
-        return $form;
-    }
+		return $form;
+	}
 
-    /**
-     * Displays a form to create a new Actor entity.
-     *
-     * @Route("/new", name="admin_actor_new")
-     * @Method("GET")
-     * @Template()
-     */
-    public function newAction()
-    {
-        $entity = new Actor();
-        $form   = $this->createCreateForm($entity);
+	/**
+	 * Displays a form to create a new Actor entity.
+	 *
+	 * @Route("/new", name="admin_actor_new")
+	 * @Method("GET")
+	 * @Template()
+	 */
+	public function newAction()
+	{
+		$entity = new Actor();
+		$form   = $this->createCreateForm($entity);
 
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
-    }
+		return array(
+			'entity' => $entity,
+			'form'   => $form->createView(),
+		);
+	}
 
-    /**
-     * Finds and displays a Actor entity.
-     *
-     * @Route("/{id}", name="admin_actor_show")
-     * @Method("GET")
-     * @Template()
-     */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
+	/**
+	 * Finds and displays a Actor entity.
+	 *
+	 * @Route("/{id}", name="admin_actor_show")
+	 * @Method("GET")
+	 * @Template()
+	 */
+	public function showAction($id)
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('SatoMoviesBundle:Actor')->find($id);
+		$entity = $em->getRepository('SatoMoviesBundle:Actor')->find($id);
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Actor entity.');
-        }
+		if (!$entity) {
+			throw $this->createNotFoundException('Unable to find Actor entity.');
+		}
 
-        $deleteForm = $this->createDeleteForm($id);
+		$deleteForm = $this->createDeleteForm($id);
 
-        return array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
+		return array(
+			'entity'      => $entity,
+			'delete_form' => $deleteForm->createView(),
+		);
+	}
 
-    /**
-     * Displays a form to edit an existing Actor entity.
-     *
-     * @Route("/{id}/edit", name="admin_actor_edit")
-     * @Method("GET")
-     * @Template()
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
+	/**
+	 * Displays a form to edit an existing Actor entity.
+	 *
+	 * @Route("/{id}/edit", name="admin_actor_edit")
+	 * @Method("GET")
+	 * @Template()
+	 */
+	public function editAction($id)
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('SatoMoviesBundle:Actor')->find($id);
+		$entity = $em->getRepository('SatoMoviesBundle:Actor')->find($id);
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Actor entity.');
-        }
+		if (!$entity) {
+			throw $this->createNotFoundException('Unable to find Actor entity.');
+		}
 
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
+		$editForm = $this->createEditForm($entity);
+		$deleteForm = $this->createDeleteForm($id);
 
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
+		return array(
+			'entity'      => $entity,
+			'edit_form'   => $editForm->createView(),
+			'delete_form' => $deleteForm->createView(),
+		);
+	}
 
-    /**
-    * Creates a form to edit a Actor entity.
-    *
-    * @param Actor $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Actor $entity)
-    {
-        $form = $this->createForm(new ActorType(), $entity, array(
-            'action' => $this->generateUrl('admin_actor_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
+	/**
+	* Creates a form to edit a Actor entity.
+	*
+	* @param Actor $entity The entity
+	*
+	* @return \Symfony\Component\Form\Form The form
+	*/
+	private function createEditForm(Actor $entity)
+	{
+		$form = $this->createForm(new ActorType(), $entity, array(
+			'action' => $this->generateUrl('admin_actor_update', array('id' => $entity->getId())),
+			'method' => 'PUT',
+		));
 
-        $form->add('submit', 'submit', array('label' => 'Update'));
+		$form->add('submit', 'submit', array('label' => 'Update'));
 
-        return $form;
-    }
-    /**
-     * Edits an existing Actor entity.
-     *
-     * @Route("/{id}", name="admin_actor_update")
-     * @Method("PUT")
-     * @Template("SatoMoviesBundle:Actor:edit.html.twig")
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
+		return $form;
+	}
+	/**
+	 * Edits an existing Actor entity.
+	 *
+	 * @Route("/{id}", name="admin_actor_update")
+	 * @Method("PUT")
+	 * @Template("SatoMoviesBundle:Actor:edit.html.twig")
+	 */
+	public function updateAction(Request $request, $id)
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('SatoMoviesBundle:Actor')->find($id);
+		$entity = $em->getRepository('SatoMoviesBundle:Actor')->find($id);
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Actor entity.');
-        }
+		if (!$entity) {
+			throw $this->createNotFoundException('Unable to find Actor entity.');
+		}
 
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
+		$deleteForm = $this->createDeleteForm($id);
+		$editForm = $this->createEditForm($entity);
+		$editForm->handleRequest($request);
 
-        if ($editForm->isValid()) {
-            $em->flush();
+		if ($editForm->isValid()) {
+			$em->flush();
 
-            return $this->redirect($this->generateUrl('admin_actor_edit', array('id' => $id)));
-        }
+			return $this->redirect($this->generateUrl('admin_actor_edit', array('id' => $id)));
+		}
 
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-    /**
-     * Deletes a Actor entity.
-     *
-     * @Route("/{id}", name="admin_actor_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
+		return array(
+			'entity'      => $entity,
+			'edit_form'   => $editForm->createView(),
+			'delete_form' => $deleteForm->createView(),
+		);
+	}
+	/**
+	 * Deletes a Actor entity.
+	 *
+	 * @Route("/{id}", name="admin_actor_delete")
+	 * @Method("DELETE")
+	 */
+	public function deleteAction(Request $request, $id)
+	{
+		$form = $this->createDeleteForm($id);
+		$form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('SatoMoviesBundle:Actor')->find($id);
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$entity = $em->getRepository('SatoMoviesBundle:Actor')->find($id);
 
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Actor entity.');
-            }
+			if (!$entity) {
+				throw $this->createNotFoundException('Unable to find Actor entity.');
+			}
 
-            $em->remove($entity);
-            $em->flush();
-        }
+			$em->remove($entity);
+			$em->flush();
+		}
 
-        return $this->redirect($this->generateUrl('admin_actor'));
-    }
+		return $this->redirect($this->generateUrl('admin_actor'));
+	}
 
-    /**
-     * Creates a form to delete a Actor entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('admin_actor_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
-    }
+	/**
+	 * Creates a form to delete a Actor entity by id.
+	 *
+	 * @param mixed $id The entity id
+	 *
+	 * @return \Symfony\Component\Form\Form The form
+	 */
+	private function createDeleteForm($id)
+	{
+		return $this->createFormBuilder()
+			->setAction($this->generateUrl('admin_actor_delete', array('id' => $id)))
+			->setMethod('DELETE')
+			->add('submit', 'submit', array('label' => 'Delete'))
+			->getForm()
+		;
+	}
 }
--- src/Sato/MoviesBundle/Controller/DirectorController.php
+++ src/Sato/MoviesBundle/Controller/DirectorController.php
@@ -18,230 +18,237 @@ use Sato\MoviesBundle\Form\DirectorType;
 class DirectorController extends Controller
 {
 
-    /**
-     * Lists all Director entities.
-     *
-     * @Route("/", name="admin_director")
-     * @Method("GET")
-     * @Template()
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+	/**
+	 * Lists all Director entities.
+	 *
+	 * @Route("/", name="admin_director")
+	 * @Method("GET")
+	 * @Template()
+	 */
+	public function indexAction(Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('SatoMoviesBundle:Director')->findAll();
+		$sql   = "SELECT a FROM SatoMoviesBundle:Director a";
+		$query = $em->createQuery($sql);
 
-        return array(
-            'entities' => $entities,
-        );
-    }
-    /**
-     * Creates a new Director entity.
-     *
-     * @Route("/", name="admin_director_create")
-     * @Method("POST")
-     * @Template("SatoMoviesBundle:Director:new.html.twig")
-     */
-    public function createAction(Request $request)
-    {
-        $entity = new Director();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
+		$paginator  = $this->get('knp_paginator');
+		$pagination = $paginator->paginate(
+			$query,
+			$request->query->get('page', 1),
+			5
+		);
+		return array(
+			'entities' => $pagination,
+		);
+	}
+	/**
+	 * Creates a new Director entity.
+	 *
+	 * @Route("/", name="admin_director_create")
+	 * @Method("POST")
+	 * @Template("SatoMoviesBundle:Director:new.html.twig")
+	 */
+	public function createAction(Request $request)
+	{
+		$entity = new Director();
+		$form = $this->createCreateForm($entity);
+		$form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($entity);
+			$em->flush();
 
-            return $this->redirect($this->generateUrl('admin_director_show', array('id' => $entity->getId())));
-        }
+			return $this->redirect($this->generateUrl('admin_director_show', array('id' => $entity->getId())));
+		}
 
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
-    }
+		return array(
+			'entity' => $entity,
+			'form'   => $form->createView(),
+		);
+	}
 
-    /**
-     * Creates a form to create a Director entity.
-     *
-     * @param Director $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Director $entity)
-    {
-        $form = $this->createForm(new DirectorType(), $entity, array(
-            'action' => $this->generateUrl('admin_director_create'),
-            'method' => 'POST',
-        ));
+	/**
+	 * Creates a form to create a Director entity.
+	 *
+	 * @param Director $entity The entity
+	 *
+	 * @return \Symfony\Component\Form\Form The form
+	 */
+	private function createCreateForm(Director $entity)
+	{
+		$form = $this->createForm(new DirectorType(), $entity, array(
+			'action' => $this->generateUrl('admin_director_create'),
+			'method' => 'POST',
+		));
 
-        $form->add('submit', 'submit', array('label' => 'Create'));
+		$form->add('submit', 'submit', array('label' => 'Create'));
 
-        return $form;
-    }
+		return $form;
+	}
 
-    /**
-     * Displays a form to create a new Director entity.
-     *
-     * @Route("/new", name="admin_director_new")
-     * @Method("GET")
-     * @Template()
-     */
-    public function newAction()
-    {
-        $entity = new Director();
-        $form   = $this->createCreateForm($entity);
+	/**
+	 * Displays a form to create a new Director entity.
+	 *
+	 * @Route("/new", name="admin_director_new")
+	 * @Method("GET")
+	 * @Template()
+	 */
+	public function newAction()
+	{
+		$entity = new Director();
+		$form   = $this->createCreateForm($entity);
 
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
-    }
+		return array(
+			'entity' => $entity,
+			'form'   => $form->createView(),
+		);
+	}
 
-    /**
-     * Finds and displays a Director entity.
-     *
-     * @Route("/{id}", name="admin_director_show")
-     * @Method("GET")
-     * @Template()
-     */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
+	/**
+	 * Finds and displays a Director entity.
+	 *
+	 * @Route("/{id}", name="admin_director_show")
+	 * @Method("GET")
+	 * @Template()
+	 */
+	public function showAction($id)
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('SatoMoviesBundle:Director')->find($id);
+		$entity = $em->getRepository('SatoMoviesBundle:Director')->find($id);
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Director entity.');
-        }
+		if (!$entity) {
+			throw $this->createNotFoundException('Unable to find Director entity.');
+		}
 
-        $deleteForm = $this->createDeleteForm($id);
+		$deleteForm = $this->createDeleteForm($id);
 
-        return array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
+		return array(
+			'entity'      => $entity,
+			'delete_form' => $deleteForm->createView(),
+		);
+	}
 
-    /**
-     * Displays a form to edit an existing Director entity.
-     *
-     * @Route("/{id}/edit", name="admin_director_edit")
-     * @Method("GET")
-     * @Template()
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
+	/**
+	 * Displays a form to edit an existing Director entity.
+	 *
+	 * @Route("/{id}/edit", name="admin_director_edit")
+	 * @Method("GET")
+	 * @Template()
+	 */
+	public function editAction($id)
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('SatoMoviesBundle:Director')->find($id);
+		$entity = $em->getRepository('SatoMoviesBundle:Director')->find($id);
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Director entity.');
-        }
+		if (!$entity) {
+			throw $this->createNotFoundException('Unable to find Director entity.');
+		}
 
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
+		$editForm = $this->createEditForm($entity);
+		$deleteForm = $this->createDeleteForm($id);
 
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
+		return array(
+			'entity'      => $entity,
+			'edit_form'   => $editForm->createView(),
+			'delete_form' => $deleteForm->createView(),
+		);
+	}
 
-    /**
-    * Creates a form to edit a Director entity.
-    *
-    * @param Director $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Director $entity)
-    {
-        $form = $this->createForm(new DirectorType(), $entity, array(
-            'action' => $this->generateUrl('admin_director_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
+	/**
+	* Creates a form to edit a Director entity.
+	*
+	* @param Director $entity The entity
+	*
+	* @return \Symfony\Component\Form\Form The form
+	*/
+	private function createEditForm(Director $entity)
+	{
+		$form = $this->createForm(new DirectorType(), $entity, array(
+			'action' => $this->generateUrl('admin_director_update', array('id' => $entity->getId())),
+			'method' => 'PUT',
+		));
 
-        $form->add('submit', 'submit', array('label' => 'Update'));
+		$form->add('submit', 'submit', array('label' => 'Update'));
 
-        return $form;
-    }
-    /**
-     * Edits an existing Director entity.
-     *
-     * @Route("/{id}", name="admin_director_update")
-     * @Method("PUT")
-     * @Template("SatoMoviesBundle:Director:edit.html.twig")
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
+		return $form;
+	}
+	/**
+	 * Edits an existing Director entity.
+	 *
+	 * @Route("/{id}", name="admin_director_update")
+	 * @Method("PUT")
+	 * @Template("SatoMoviesBundle:Director:edit.html.twig")
+	 */
+	public function updateAction(Request $request, $id)
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('SatoMoviesBundle:Director')->find($id);
+		$entity = $em->getRepository('SatoMoviesBundle:Director')->find($id);
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Director entity.');
-        }
+		if (!$entity) {
+			throw $this->createNotFoundException('Unable to find Director entity.');
+		}
 
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
+		$deleteForm = $this->createDeleteForm($id);
+		$editForm = $this->createEditForm($entity);
+		$editForm->handleRequest($request);
 
-        if ($editForm->isValid()) {
-            $em->flush();
+		if ($editForm->isValid()) {
+			$em->flush();
 
-            return $this->redirect($this->generateUrl('admin_director_edit', array('id' => $id)));
-        }
+			return $this->redirect($this->generateUrl('admin_director_edit', array('id' => $id)));
+		}
 
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-    /**
-     * Deletes a Director entity.
-     *
-     * @Route("/{id}", name="admin_director_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
+		return array(
+			'entity'      => $entity,
+			'edit_form'   => $editForm->createView(),
+			'delete_form' => $deleteForm->createView(),
+		);
+	}
+	/**
+	 * Deletes a Director entity.
+	 *
+	 * @Route("/{id}", name="admin_director_delete")
+	 * @Method("DELETE")
+	 */
+	public function deleteAction(Request $request, $id)
+	{
+		$form = $this->createDeleteForm($id);
+		$form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('SatoMoviesBundle:Director')->find($id);
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$entity = $em->getRepository('SatoMoviesBundle:Director')->find($id);
 
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Director entity.');
-            }
+			if (!$entity) {
+				throw $this->createNotFoundException('Unable to find Director entity.');
+			}
 
-            $em->remove($entity);
-            $em->flush();
-        }
+			$em->remove($entity);
+			$em->flush();
+		}
 
-        return $this->redirect($this->generateUrl('admin_director'));
-    }
+		return $this->redirect($this->generateUrl('admin_director'));
+	}
 
-    /**
-     * Creates a form to delete a Director entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('admin_director_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
-    }
+	/**
+	 * Creates a form to delete a Director entity by id.
+	 *
+	 * @param mixed $id The entity id
+	 *
+	 * @return \Symfony\Component\Form\Form The form
+	 */
+	private function createDeleteForm($id)
+	{
+		return $this->createFormBuilder()
+			->setAction($this->generateUrl('admin_director_delete', array('id' => $id)))
+			->setMethod('DELETE')
+			->add('submit', 'submit', array('label' => 'Delete'))
+			->getForm()
+		;
+	}
 }
