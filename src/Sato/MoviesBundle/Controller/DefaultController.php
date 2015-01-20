eceb5bb195a5a1137120aa47fbb0e1360813c458
Luciana Sato <sato.lu@gmail.com>
2015-01-20 00:46:29 -0200

    Adicionado página de Contato
    + Criação da rota
    + Formulário
    + Controller

--- src/Sato/MoviesBundle/Controller/DefaultController.php
+++ src/Sato/MoviesBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Sato\MoviesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sato\MoviesBundle\Entity\Contact;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -10,8 +12,26 @@ class DefaultController extends Controller
 	{
 		$entity_manager = $this->getDoctrine()->getManager();
 		$movies = $entity_manager->getRepository('SatoMoviesBundle:Movie')->findAll();
+		
 		return $this->render('SatoMoviesBundle:Default:index.html.twig', array(
 			'movies' => $movies
 		));
+	}
+
+	public function contactAction(Request $request)
+	{
+        $contact = new Contact();
+        $contact->setName('Luciana');
+        $contact->setMessage('Message');
+
+        $form = $this->createFormBuilder($contact)
+            ->add('name', 'text')
+            ->add('message', 'text')
+            ->add('save', 'submit', array('label' => 'Send'))
+            ->getForm();
+
+        return $this->render('SatoMoviesBundle:Default:contact.html.twig', array(
+            'form' => $form->createView(),
+        ));
 	}
 }
---
+++ src/Sato/MoviesBundle/Entity/Contact.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sato\MoviesBundle\Entity;
+
+class Contact
+{
+    protected $name;
+    protected $message;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+}
