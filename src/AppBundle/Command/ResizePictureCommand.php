b3176e929be2b41558576d2bd62fcafa25e2c4aa
Luciana Sato <sato.lu@gmail.com>
2015-02-21 22:52:41 -0200

    [Log] Adicionar log
    + Adicionar log ao command console

--- src/AppBundle/Command/ResizePictureCommand.php
+++ src/AppBundle/Command/ResizePictureCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Psr\Log\LoggerInterface;
 
 class ResizePictureCommand extends ContainerAwareCommand
 {
@@ -41,6 +42,9 @@ class ResizePictureCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        // Log service
+        $logger = $this->getContainer()->get('logger');
+
         $path = self::DEFAULT_INPUT_FOLDER . $input->getArgument('path');
         $size = $input->getOption('size') ? $input->getOption('size') : self::DEFAULT_SIZE_IMAGE ;
         $out = $input->getOption('out') ? $input->getOption('out') : self::DEFAULT_OUTPUT_FOLDER ;
@@ -53,11 +57,13 @@ class ResizePictureCommand extends ContainerAwareCommand
 
             $image->resize($box)->save($out . $filename);
             $output->writeln(sprintf('Path: %s - Folder output: %s', $path, $out));
+            // write a log
+            $logger->info(sprintf('Path: %s - Folder output: %s', $path, $out));
         }
         else {
             $output->writeln('<info>File not found </info>');
+            // write a log
+            $logger->warning('File not found');
         }
-
-
     }
 }
